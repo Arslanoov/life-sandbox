57622e4ab0cb3b903000c9cb204611d4e8b0a1a7
RasulArslanov <rasularslanoov@gmail.com>
2020-09-20 19:17:25 +0500

    Fixed bugs

---
+++ public/geometry/rectangle/Rectangle.js
@@ -0,0 +1,20 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class Rectangle {
+    constructor(startX, startY, width, height) {
+        this.startX = startX;
+        this.startY = startY;
+        this.width = width;
+        this.height = height;
+    }
+    draw(context) {
+        context.strokeRect(this.startX, this.startY, this.width, this.height);
+    }
+    moveUp(coordinate) {
+        this.startY -= coordinate;
+    }
+    moveDown(coordinate) {
+        this.startY += coordinate;
+    }
+}
+exports.default = Rectangle;
